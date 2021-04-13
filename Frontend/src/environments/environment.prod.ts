9983354af8ab6d0c92d8dbe22399c322a3ed863a
CHRIS GRANT <admin@chrisgrant.onmicrosoft.com>
2021-04-13 19:02:36 +0000

    Updated environment.prod.ts

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true
   // api_url: "http://kong-proxy.kong.svc.cluster.local/dataapi/api/values"
+  // api_url: "http://kong-proxy.default.svc.cluster.local/dataapi/api/values"
 };
