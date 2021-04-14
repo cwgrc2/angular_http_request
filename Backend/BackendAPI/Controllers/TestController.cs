2e921d950674a5a321f229a6083c90d699f1b752
bill.bashore <bill.bashore@gmail.com>
2021-04-14 14:12:15 -0600

    Bill adds Backend

---
+++ Backend/BackendAPI/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+
+namespace BackendAPI.Controllers
+{
+    [Route("test/api/[controller]")]
+    [ApiController]
+    public class TestController : Controller
+    {
+        // GET api/test
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Successfully pinged Test controller!!" };
+        }
+    }
+}
